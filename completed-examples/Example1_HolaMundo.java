2035491f07478001772b8b5fe74214093f4df40e
Estefano Solis <estefano.sebastian.solis.campos@gmail.com>
2025-11-15 01:16:04 -0500

    feat: Added missing example (HolaMundo)

--- completed-examples/Example1_HolaMundo.java
+++ completed-examples/Example1_HolaMundo.java
@@ -1,3 +1,7 @@
-public class Leccion1_HolaMundo {
 
+public class Leccion1_HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir un mensaje en consola
+        System.out.println("¡Hola Mundo!");
+    }
 }
