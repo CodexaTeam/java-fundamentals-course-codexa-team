7ffa49e55b8f371c1b24c473889cbba0401e0d60
Sergio L.Dev <sergio.javier.landa.ortiz@gmail.com>
2025-11-15 23:53:23 -0500

    add files examples 1 ,2 y 3

---
+++ started-files/Example3_EstructurasControl_Starter.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Leccion3_EstructurasControl_Starter {
+    public static void main(String[] args) {
+        // Crea un objeto Scanner para leer datos del usuario
+        Scanner scanner = new Scanner(System.in);
+        
+        // Pide al usuario que ingrese un número
+
+        // Usa if-else para verificar si el número es positivo, negativo o cero
+
+        // Usa un bucle para contar hasta 5
+
+        // Cierra el scanner
+        scanner.close();
+    }
+}
