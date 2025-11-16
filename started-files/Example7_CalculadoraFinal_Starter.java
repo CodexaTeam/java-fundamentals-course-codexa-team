3d4b7a0a242c7e31f2aba5ce9dce6675580deb98
JesusRodriguez7 <89036050+JesusRodriguez7@users.noreply.github.com>
2025-11-15 20:28:55 -0500

    feat: complete example number 7 (Calculadora final)

---
+++ started-files/Example7_CalculadoraFinal_Starter.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Leccion6_CalculadoraFinal_Starter {
+    
+    // Atributos de la clase
+
+    // Métodos para cada operación (suma, resta, multiplicación, división)
+
+    public static void main(String[] args) {
+        // Pide al usuario que ingrese dos números
+
+        // Muestra el menú de operaciones y ejecuta la operación seleccionada
+
+        // Cierra el scanner
+    }
+}
