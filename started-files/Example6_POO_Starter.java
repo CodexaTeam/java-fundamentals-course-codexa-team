77e6dd63bfab80c1f0095618ddf44f2890be6e9e
JesusRodriguez7 <89036050+JesusRodriguez7@users.noreply.github.com>
2025-11-15 20:28:08 -0500

    feat: complete example number 6 (POO)

---
+++ started-files/Example6_POO_Starter.java
@@ -0,0 +1,31 @@
+// Clase Auto
+class Auto {
+    // Atributos
+    String color;  // Asigna el color del auto
+    String marca;  // Asigna la marca del auto
+    int velocidad; // Asigna la velocidad del auto
+
+    // Métodos
+    public void acelerar() {
+        // TODO: Completa el código para incrementar la velocidad del auto en 10
+    }
+
+    public void frenar() {
+        // TODO: Completa el código para detener el auto, es decir, poner la velocidad a 0
+    }
+}
+
+public class Leccion05_Auto {
+    public static void main(String[] args) {
+        // Sintaxis para crear objetos
+        // TODO: Crea un objeto de la clase Auto llamado "carro1"
+
+        // Asignar valores a los atributos
+        // TODO: Asigna el color, la marca y la velocidad inicial del auto
+
+        // Usar sus métodos
+        // TODO: Llama al método acelerar() para incrementar la velocidad
+        // TODO: Imprime la velocidad del auto en la consola
+    }
+}
+
